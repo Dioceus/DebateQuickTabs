f51fb042a7fa42548041051e600a4ab76fd3ac31
Dioceus <chuwei.chen2001@gmail.com>
2018-12-09 16:30:33 -0500

    Fixing IOExceptions

--- app/src/main/java/com/example/david/debatequicktabs/AdjunicatorActivity.java
+++ app/src/main/java/com/example/david/debatequicktabs/AdjunicatorActivity.java
@@ -68,23 +68,14 @@ import org.json.JSONObject;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
-import com.google.api.services.sheets.v4.Sheets;
-import com.google.api.services.sheets.v4.SheetsScopes;
-import com.google.api.services.sheets.v4.model.ValueRange;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import java.util.Collections;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import com.google.api.client.auth.oauth2.Credential;
 
+
 public class AdjunicatorActivity extends Activity {
+
     GoogleAccountCredential mCredential;
     private TextView mOutputText;
     private Button mCallApiButton;
@@ -117,9 +108,8 @@ public class AdjunicatorActivity extends Activity {
      */
 
 
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
         EditText urlInput = (EditText) findViewById(R.id.spreadsheetURL);
@@ -172,7 +162,13 @@ public class AdjunicatorActivity extends Activity {
             public void onClick(View v) {
 
                 final NetHttpTransport HTTP_TRANSPORT = new com.google.api.client.http.javanet.NetHttpTransport();
-                final String spreadsheetId = sheetURL.substring(38,82);
+                final String spreadsheetId = sheetURL.substring(38, 82);
+
+                //postData needs parameters, govTeam, oppTeam, HTTP_Transport, and spreadsheetId
+                postData(govTeam, oppTeam, spreadsheetId, HTTP_TRANSPORT);
+
+
+    } public void postData(Team govTeam, Team oppTeam, String spreadsheetId, final NetHttpTransport HTTP_TRANSPORT) throws IOException{
                 //Range for speakers: A2:G
                 //Range for teams: A2:F
                 final String range = "Raw Speaker Data: A2:G";
@@ -181,16 +177,17 @@ public class AdjunicatorActivity extends Activity {
                         .build();
                 ValueRange response = service.spreadsheets().values()
                         .get(spreadsheetId, range).execute();
+
                 List<List<Object>> values = response.getValues();
 
                 for (int i = 0; i < 4; i++) {
-                    for (List row: values) {
+                    for (List row : values) {
                         if (i == 1) {
                             if (row.equals(govTeam.speakerNames[0].toLowerCase())) {
                                 //UpdateValuesResponse result =
-                                       // service.spreadsheets().values().update(spreadsheetId, range)
-                                           //     .setValueInputOption(valueInputOption)
-                                           //     .execute();
+                                // service.spreadsheets().values().update(spreadsheetId, range)
+                                //     .setValueInputOption(valueInputOption)
+                                //     .execute();
                             } else if (row.equals(govTeam.speakerNames[1].toLowerCase())) {
 
                             } else if (row.equals(oppTeam.speakerNames[0].toLowerCase())) {
@@ -203,7 +200,7 @@ public class AdjunicatorActivity extends Activity {
                 }
 
                 for (int j = 0; j < 2; j++) {
-                    for (List row: values) {
+                    for (List row : values) {
                         if (j == 1) {
                             if (row.equals(govTeam.teamName.toLowerCase())) {
 
@@ -217,9 +214,8 @@ public class AdjunicatorActivity extends Activity {
             }
 
         });
-
     }
-    private static Credential getCredentials (final NetHttpTransport HTTP_TRANSPORT) throws
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws
             IOException {
         // Load client secrets.
         InputStream in = AdjunicatorActivity.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
@@ -234,5 +230,6 @@ public class AdjunicatorActivity extends Activity {
         LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
+
 }
 
